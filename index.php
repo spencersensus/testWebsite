4c287e0c6664236b8644196c38f65ae23dd9d023
spencersensus <spencerwoods@Spencers-MacBook-Pro.local>
2018-12-18 09:18:53 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+CTYPE html>
+<html>
+
+<head>
+  <link href="HomeStyle.css" rel="stylesheet" type="text/css" />
+  <meta charset="utf-8" />
+  <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  
+  <title></title>
+</head>
+
+<body>
+<?php
+session_start();
+?>
+
+<p>HELLO WORLD </p>
+</body>
+</html>
+
